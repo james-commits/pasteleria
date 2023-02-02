d9bb305df89ae8094d39cdff79080832e91c1ffa
James <valmonth15@gmail.com>
2023-02-02 12:44:19 -0600

    Upgrade0223

--- admin/pages/galerias/galeria.php
+++ admin/pages/galerias/galeria.php
@@ -5,24 +5,40 @@ if (isset($_FILES['image']) && isset($_POST['name'])) {
 
   // Verifica si la imagen se subió correctamente
   if ($image['error'] === UPLOAD_ERR_OK) {
-    $directory = 'img/primeraComunion/';
+    $directory = 'img';
     $imagePath = $directory . '/' . $image['name'];
     move_uploaded_file($image['tmp_name'], $imagePath);
 
-    // Agrega el objeto al archivo JSON
+    // Lee el contenido del archivo JSON
     $data = [];
-    if (file_exists('../js/data.json')) {
-      $json = file_get_contents('../js/data.json');
+    if (file_exists('data.json')) {
+      $json = file_get_contents('data.json');
       $data = json_decode($json, true);
     }
+   
+    $mainstr = $image['name'];
+    $str = $mainstr;
+    $str1 = substr($str, 1);
+    $sig = '/';
+       
+    $ruta = 'img'.$sig.$str;
+    $str3 = str_replace("\\\\", '/', $ruta);
+    // Agrega el nuevo objeto al array
     $data[] = [
       'name' => $name,
-      'image' => $imagePath,
+      'img' => $str3,
     ];
+
+    // Codifica el array a una cadena JSON y escriba la cadena en el archivo JSON
     file_put_contents('data.json', json_encode($data));
+    
+ 
+    header("Location: primera.html");
+    echo "Exito";
   } else {
     // Maneja el error de subida de imagen
     // ...
+    
   }
 }
 ?>
---
+++ admin/pages/galerias/quince.js
@@ -0,0 +1,38 @@
+function myFunction() {
+    fetch ("data.json")
+    .then(response => {
+        return response.json();
+        }
+    )
+    .then(jsondata => {
+        myData = jsondata;
+        console.log(myData);
+        console.log(myData[0].img);
+        html = "";
+        for (let i = 0; i < myData.length; i++) {
+            html += `
+            <div class="col-md-4 col-6 cake bakery bread">
+            <div class="portfolio_item">
+                <div class="portfolio_img">
+                    <a class="light" href="${myData[i].img}">
+                        <img class="img-fluid" src="${myData[i].img}" alt="">
+                    </a>
+                </div>
+                <div class="portfolio_text">
+                    <a href="#"><h4>${myData[i].name}</h4></a>
+                </div>
+            </div>
+        </div>`;
+                    
+        }
+        document.getElementById("comunion").innerHTML = html;
+        
+    
+    }
+    )
+    .catch(error => console.log(error));
+    
+
+};
+
+myFunction();
